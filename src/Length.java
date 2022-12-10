6ccc925a71415c9342e54c76edb75d86ffffd6bb
leo <e225709@ie.u-ryukyu.ac.jp>
2022-12-11 00:13:19 +0900

    step3

--- src/Length.java
+++ src/Length.java
@@ -1,11 +1,8 @@
 public class Length{
     public static void main(String[] args){
-        try{
         String str;
-        str = null;
-        System.out.println(str.length());
-        }catch (java.lang.NullPointerException e) {
-            System.out.println("例外NullPointerExceptionが発生しました。" + e.getMessage());
+        str = "壱百満";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
         }
     }
-}
